08059e67b52ac5071dad33f675ff0d238e6c060a
gc201xhc <78838583+gc201xhc@users.noreply.github.com>
2021-02-09 23:30:37 -0600

    Add files via upload

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -24,13 +24,13 @@ let setFruit = {
 let reverseLevelUp = false;
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.3;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://ae04.alicdn.com/kf/H802d2f030b82489cabad3a7eca7493dep.jpg';
+let adLink = 'https://space.bilibili.com/394076204';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '火车酱来了';
